85cfec7f46c836c14ee6b3598a18c4aa90b5e063
Partha Goswami <partha.ext@gmail.com>
2022-03-08 14:46:04 +0530

    Added Study Materials and Code Changes

--- SpringRest/src/main/java/com/cp/web/ProductController.java
+++ SpringRest/src/main/java/com/cp/web/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cp.model.Product;
@@ -22,8 +23,18 @@ public class ProductController {
 	
 	@PostMapping("/product")
     public ResponseEntity addProduct(@RequestBody Product product) {
-		productList.add(product);
-		return ResponseEntity.created(null).build();
+		try {
+			
+			if(product.getId() == 000) {
+				throw new Exception("Something Is Wrong");
+			}
+			
+			productList.add(product);
+			
+		}catch(Exception e) {
+			return ResponseEntity.status(400).build();
+		}		
+		return ResponseEntity.created(null).build();		
     }
   
     @GetMapping("/product")
@@ -34,6 +45,10 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProductById(@PathVariable long id) {
     	
+    	if(id == 000) {
+    		return ResponseEntity.status(400).build();
+    	}
+    	    	
         return ResponseEntity.ok(productList.stream()
         		.filter(p -> p.getId() == id)
         		.findFirst()
@@ -41,7 +56,7 @@ public class ProductController {
     }
   
     @DeleteMapping("/product/{id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable long id , @RequestParam String name) {
     	productList.removeIf(p -> p.getId() == id);
     	return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
